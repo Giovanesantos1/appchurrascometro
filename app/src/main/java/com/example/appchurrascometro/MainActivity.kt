f6fe65ff676d54bb70fb5725fd5537469eee196c
GiovaneDev <giovane.santosdm@gmail.com>
2024-10-28 21:54:17 +0000

    recuperando dados do usuario

--- app/src/main/java/com/example/appchurrascometro/MainActivity.kt
+++ app/src/main/java/com/example/appchurrascometro/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.appchurrascometro
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val edtNumAdult = findViewById<TextInputEditText>(R.id.edt_number_of_adult)
+        val edtNumkids = findViewById<TextInputEditText>(R.id.edt_number_of_kids)
+        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener {
+
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
